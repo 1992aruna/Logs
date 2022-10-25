ec09db50fa7184b9eeef03624769a8efc0c8e4f3
1992aruna <113091758+1992aruna@users.noreply.github.com>
2022-10-25 18:43:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/user-routes");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api", router);
+mongoose
+.connect (
+    "mongodb+srv://admin:Aruna@1992@cluster0.6eeqzda.mongodb.net/auth?retryWrites=true&w=majority"
+    )
+.then(() => {
+app.listen(5000);
+console.log("Database is connected! Listening to localhost 5000");
+})
+.catch((err) => console.log(err));
